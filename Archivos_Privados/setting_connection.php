be97008b2c587ceef9c7d334c89bc0151d12fa6a
yorman <sanchezyorman0@gmail.com>
2022-12-05 21:24:20 -0500

    VERSION DEL SOFTWARE V0.0.10

---
+++ Archivos_Privados/setting_connection.php
@@ -0,0 +1,10 @@
+<?php
+const IP='localhost';
+const DATA_BASE='mis_tejidos';
+
+const USER_SEL='postgres';
+const PASSWORD_SEL='Profeta2';
+
+const USER_ALL='postgres';
+const PASSWORD_ALL='Profeta2';
+?>
